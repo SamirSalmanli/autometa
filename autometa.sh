1b75ef55223042db840440879dff2854d804f17d
Samir <87500365+SamirSalmanli@users.noreply.github.com>
2021-07-29 12:36:22 +0300

    Add files via upload

---
+++ autometa.sh
@@ -0,0 +1,17 @@
+pkg install figlet -y
+figlet AUTOMETA
+pkg update && pkg upgrade -y
+figlet AUTOMETA
+pkg install wget curl openssh git -y
+figlet AUTOMETA
+apt install ncurses-utils
+figlet AUTOMETA
+git clone https://github.com/OnlineHacKing/Metasploit_Termux
+figlet AUTOMETA
+cd Metasploit_Termux
+figlet AUTOMETA
+chmod +x metasploit.sh
+figlet AUTOMETA
+./metasploit.sh
+chmod +x *
+figlet AUTOMETA
